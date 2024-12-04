61cc3774973defab218e05483a44a7b5d12a7513
lingkerio <lingkerio@qq.com>
2024-12-04 13:39:34 +0800

    feat: add template for unionfind

---
+++ template/UnionFind.h
@@ -0,0 +1,39 @@
+#include <vector>
+
+using namespace std;
+
+class UnionFind {
+ private:
+  vector<int> parent, rank;
+
+ public:
+  UnionFind(int n) : parent(n), rank(n, 0) {
+    for (int i = 0; i < n; ++i) {
+      parent[i] = i;
+    }
+  }
+
+  int find(int x) {
+    if (parent[x] != x) {
+      parent[x] = find(parent[x]);
+    }
+    return parent[x];
+  }
+
+  bool unite(int x, int y) {
+    int rootX = find(x);
+    int rootY = find(y);
+    if (rootX == rootY) {
+      return false;
+    }
+    if (rank[rootX] > rank[rootY]) {
+      parent[rootY] = rootX;
+    } else if (rank[rootX] < rank[rootY]) {
+      parent[rootX] = rootY;
+    } else {
+      parent[rootY] = rootX;
+      rank[rootX]++;
+    }
+    return true;
+  }
+};
